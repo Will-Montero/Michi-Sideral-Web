6fdf99be5c1d78a78a802eb5a69aaec89a166f1e
Will-Montero <monteromora99@gmail.com>
2024-08-06 16:46:43 -0500

    card

--- src/AppMichi.jsx
+++ src/AppMichi.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header"
 import { Videos } from "./pages/Videos"
+import { Novedades } from "./pages/Novedades"
 export const AppMichi = () => {
   return (
     <>
     <Header></Header>
     <Videos></Videos>
+    <Novedades></Novedades>
     </>
   )
 }
--- src/pages/Novedades.jsx
+++ src/pages/Novedades.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import loopEvento from '../assets/images/loopEvento.avif'
+import '../styles/novedades.css'
 export const Novedades = () => {
   return (
-    <div>Novedades</div>
+    <>
+    <a>
+    <div className="card">
+    <img src={loopEvento} className="card-img-top"/>
+  <div className="card-body">
+    <h6 className="card-text">Nuestro próximo lanzamiento</h6>
+    <h3>Loop</h3>
+     </div>
+   </div>
+    </a>
+    </>
   )
 }
