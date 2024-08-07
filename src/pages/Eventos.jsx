25edf662528f5727620f450c25ea30d25751985f
Will-Montero <monteromora99@gmail.com>
2024-08-07 11:45:56 -0500

    jsx

---
+++ src/pages/Eventos.jsx
@@ -0,0 +1,22 @@
+
+export const Eventos = () => {
+  return (
+    <section className="containerEventos">
+  <div className="tituloEventos">
+    <h2>Eventos!</h2>
+  </div>
+  <br/>
+  <br/>
+ <main>
+  <div className="cardEventos">
+    <img src="./img/eventos/paramoreToque.jpeg" alt=""/>
+    <h5>Recuerdo de uno de nuestros toque, haciendo un buen tributo a Paramore</h5>
+  </div>
+  <div className="cardEventos">
+    <img src="./img/eventos/nirvanaToque.jpeg" alt=""/>
+    <h5>inicia como una idea y se hizo realidad, tributo a nirvana</h5>
+  </div>
+ </main>
+</section>
+  )
+}
