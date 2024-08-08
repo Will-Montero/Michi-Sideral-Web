56736b88ee359663be5dd695705b738f86818db9
Will-Montero <monteromora99@gmail.com>
2024-08-07 19:00:37 -0500

    jsx merch

--- src/pages/Merch.jsx
+++ src/pages/Merch.jsx
@@ -1,6 +1,26 @@
 
 export const Merch = () => {
   return (
-    <div>Merch</div>
+    <section className="Merch">
+  <header className="headerMerch">
+    <h2 className="tituloMerch">Merch...</h2>
+  </header>
+  <div className="containerMerh">
+    <div className="image-container">
+        <img id="product-image"  alt="Product Image"/>
+    </div>
+    <div className="controls">
+        <button id="prev-btn">⬅️</button>
+        <div className="product-info" style="background: none;">
+            <h7 id="product-name">Product 1</h7>
+            <p id="product-price">$100</p>
+            <button  className="botonComprar">comprar</button>
+        </div>
+        <button  className="botonMerch" id="next-btn">➡️</button>
+    </div>
+
+  </div>
+</section>
+ 
   )
 }
