52f95709bb74a1e131b747519ea55dfbac920026
Will-Montero <monteromora99@gmail.com>
2024-08-08 11:09:50 -0500

    target_blank

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -12,7 +12,7 @@ export const Footer = () => {
    <section>
     <nav className="iconFooter">
       <a href=""><img src={facebook} alt=""/></a>
-      <a href="https://www.instagram.com/michisideral/"><img src={instagram} target="black" alt=""/></a>
+      <a href="https://www.instagram.com/michisideral/" target="_blank"><img src={instagram} /></a>
       <a href=""><img src={spotify} alt=""/></a>
       <a href=""><img src={tiktok} alt=""/></a>
       <a href=""><img src={gmail} alt=""/></a>
--- src/pages/Logo.jsx
+++ src/pages/Logo.jsx
@@ -5,7 +5,9 @@ import LogoHome from '../assets/images/LogoHome.png';
 export const Logo = () => {
   return (
     <div className='div'>
+      <br />
        <h1 className="content">Michi Sideral</h1>
+       <br />
       <img className='imagenLogo' src={LogoHome}  ></img>
        
     </div>
