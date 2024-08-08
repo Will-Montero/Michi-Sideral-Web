9a759e49b697590f136f38ffc368e75613ef0e31
Will-Montero <monteromora99@gmail.com>
2024-08-07 19:20:26 -0500

    merch

---
+++ src/hook/merch.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        name: 'Pegatina Sideral',
+        price: '$20.000',
+        image: 'img/StikerGato.jpeg'
+    }, 
+    {
+        name: 'Camiseta',
+        price: '$30.000',
+        image: 'img/camisetaGato.jpeg'
+    },
+    {
+        name: 'Michi Botones',
+        price: '$5.000',
+        image: 'img/botonGato.jpeg'
+    }
+];
+
+let currentIndex = 0;
+
+const productName = document.getElementById('product-name');
+const productPrice = document.getElementById('product-price');
+const productImage = document.getElementById('product-image');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+function updateProductDisplay() {
+    productName.textContent = products[currentIndex].name;
+    productPrice.textContent = products[currentIndex].price;
+    productImage.src = products[currentIndex].image;
+}
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : products.length - 1;
+    updateProductDisplay();
+});
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex < products.length - 1) ? currentIndex + 1 : 0;
+    updateProductDisplay();
+});
+
+// Initialize display
+updateProductDisplay();
--- src/pages/Merch.jsx
+++ src/pages/Merch.jsx
@@ -1,26 +1,26 @@
-import '../styles/merch.css'
+import "../styles/merch.css";
 export const Merch = () => {
   return (
     <section className="Merch">
-  <header className="headerMerch">
-    <h2 className="tituloMerch">Merch...</h2>
-  </header>
-  <div className="containerMerh">
-    <div className="image-container">
-        <img id="product-image"  alt="Product Image"/>
-    </div>
-    <div className="controls">
-        <button id="prev-btn">⬅️</button>
-        <div className="product-info" style="background: none;">
-            <h7 id="product-name">Product 1</h7>
-            <p id="product-price">$100</p>
-            <button  className="botonComprar">comprar</button>
+      <header className="headerMerch">
+        <h2 className="tituloMerch">Merch...</h2>
+      </header>
+      <div className="containerMerh">
+        <div className="image-container">
+          <img id="product-image" alt="Product Image" />
         </div>
-        <button  className="botonMerch" id="next-btn">➡️</button>
-    </div>
-
-  </div>
-</section>
- 
-  )
-}
+        <div className="controls">
+          <button id="prev-btn">⬅️</button>
+          <div className="product-info">
+            <h6 id="product-name">Product 1</h6>
+            <p id="product-price">$100</p>
+            <button className="botonComprar">comprar</button>
+          </div>
+          <button className="botonMerch" id="next-btn">
+            ➡️
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
